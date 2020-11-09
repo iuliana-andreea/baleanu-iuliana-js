86e488e6709768d89e2fc6c9828a483457d42e00
Iuliana Baleanu <baleanu.iuliana25@gmail.com>
2020-11-09 16:31:54 +0200

    Tema 8 Oct Curs03

---
+++ M01S03/ex01/app.js
@@ -0,0 +1,24 @@
+console.log(object.surname);
+console.log(object.age);
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+// Afiseaza anul in care aveai jumatate din varsta pe care ai trecut-o
+// in obiect scazand din anul curent (un proces numit hardcodare)
+console.log(2020 - object.age / 2);
+
+// Tema
+
+// Afiseaza fraza “xxx yyy are xx ani.” in consola folosind operatorul de concatenare
+console.log(object.name + ' ' + object.surname + ' are ' + object.age + ' ani');
+
+// Afiseaza anul in care te-ai nascut scazand varsta din anul curent
+console.log(2020 - object.age);
