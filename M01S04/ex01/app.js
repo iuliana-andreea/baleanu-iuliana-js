44523ff3b6e745a8adfd04f4991579b722293ead
Iuliana Baleanu <baleanu.iuliana25@gmail.com>
2020-11-09 17:13:47 +0200

    Tema 15 Oct Curs 04

--- M01S04/ex01/app.js
+++ M01S04/ex01/app.js
@@ -23,3 +23,22 @@ if (value % 2 === 0) {
 
 var message = `Valoarea este ${value} si este ${word}`;
 messageParagraph.innerText = message;
+
+//TEMA
+// Tema
+// Folosind metoda prompt() afiseaza in pagina mesajul:
+// “Numarul introdus este mai mic | mai mare decat 20.”
+let word2 = 'mai mic';
+
+if (value < 20 && !isNaN(value) && parseInt(value) !== null) {
+  word2 = 'mai mic';
+} else if (value > 20 && !isNaN(value) && parseInt(value) !== null) {
+  word2 = 'mai mare';
+} else if (value === 20 && !isNaN(value) && parseInt(value) !== null) {
+  word2 = 'egal';
+} else {
+  word2 = 'incorect input';
+}
+let message2 = `Numarul introdus este ${word2} decat 20`;
+
+secondMessageParagraph.textContent = message2;
--- M01S04/ex04/app.js
+++ M01S04/ex04/app.js
@@ -7,8 +7,46 @@ var i;
 //   }
 // }
 
-for (i = 1; i < 100; i++) {
-  if (i % 2 !== 0) {
+// for (i = 1; i < 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+//TEMA
+// Modifica exemplul astfel incat bucla sa numere de la 1 la 89.
+for (i = 1; i < 90; i++) {
+  console.log(i);
+}
+
+// Modifica exemplul astfel incat bucla sa numere de la 1 la 88 .
+for (i = 1; i < 89; i++) {
+  console.log(i);
+}
+
+// Folosind keywordul break opreste bucla atunci cand numarul este egal cu 42.
+for (i = 1; i < 89; i++) {
+  console.log(i);
+
+  if (i === 42) {
+    break;
+  }
+}
+
+// Folosind keywordul continue, afiseaza doar numele impare intre 1 si 89.
+for (i = 1; i < 90; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log(i);
+}
+
+// In mod similar, afiseaza doar numerele care sunt divizibile cu 3.
+for (i = 1; i <= 100; i++) {
+  if (i % 3 !== 0) {
     continue;
   }
 
