2da1f722cb3e0145359b81fc2afcce131a85a91a
Iuliana Baleanu <baleanu.iuliana25@gmail.com>
2020-11-09 17:18:36 +0200

    Curs 04 Ex 5,6,12,13

---
+++ M01S04/ex13/app.js
@@ -0,0 +1,12 @@
+var globalVariable = 'Maaflu in contextul global';
+
+if (true) {
+  var localVariable = 'Ma aflu in contextul local';
+  var globalVariable = 'Nu ma aflu in contextul global';
+
+  // console.log(globalVariable);
+  // console.log(localVariable);
+}
+
+console.log(globalVariable);
+//console.log(localVariable);
